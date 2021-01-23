c4e6bcc81c40cdc2cef43313267d0fac822722aa
adiss01 <69195521+adiss01@users.noreply.github.com>
2021-01-23 18:49:55 +0300

    Update server.lua

--- server.lua
+++ server.lua
@@ -5,12 +5,14 @@ local aktif = false
 
 ESX.RegisterUsableItem('bodycam', function(source)
 	local xPlayer = ESX.GetPlayerFromId(source)
-	if xPlayer.job.name == 'police' or xPlayer.job.name == 'ambulance' or xPlayer.job.name == 'sheriff' then
+	if xPlayer.job.name == 'police' or xPlayer.job.name == 'ambulance' or xPlayer.job.name == 'sheriff' and not aktif then
 		local data = isimcek(source)
+		aktif = true
 		TriggerClientEvent('bodycam:show', source, data.firstname.. " " .. data.lastname, " " ..  xPlayer.job.grade_label)
 		--TriggerClientEvent('mythic_notify:client:SendAlert', xPlayer.source, { type = 'inform', text = "Bodycam açıldı."})	
 	else
 		TriggerClientEvent('mythic_notify:client:SendAlert', xPlayer.source, { type = 'error', text = "Polis veya doktor olmadan kullanamazsın."})
+		aktif = false
 	end
 end)
 
@@ -28,3 +30,17 @@ function isimcek(source)
 		return nil
 	end
 end
+
+AddEventHandler('esx:onRemoveInventoryItem', function(source, item, count)
+    local _source = source
+    local xPlayer = ESX.GetPlayerFromId(_source)
+    if xPlayer ~= nil and xPlayer.job ~= nil and xPlayer.job.name == 'police' or xPlayer.job.name == 'ambulance' or xPlayer.job.name == 'sheriff' then
+		if item.name == 'bodycam' and item.count < 1 then
+			if aktif then
+				aktif = false
+				TriggerClientEvent('bodycam:close', _source)
+				TriggerClientEvent('mythic_notify:client:SendAlert', xPlayer.source, { type = 'error', text = "Bodycam kapatıldı."})
+			end
+        end
+    end
+end)
